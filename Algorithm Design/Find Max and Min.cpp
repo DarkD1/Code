5ae960d5d850167e5a0c9d23820861777d2637db
SazzadShopno <Sazzadshopno6@gmail.com>
2018-12-01 18:49:42 +0600

    new

--- Algorithm Design/Find Max and Min.cpp
+++ Algorithm Design/Find Max and Min.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+int mx, mn, mx1, mn1;
+void fMaxMin(int *a, int i, int j, int &mx ,int &mn){
+    if(i == j) mx = mn = a[i];
+    else if(i == j - 1){
+        a[i] > a[j]? mx = a[i], mn = a[j] : mn = a[i], mx = a[j];
+    }
+    else {
+        fMaxMin(a, i, (i+j)/2, mx, mn);
+        fMaxMin(a,((i+j)/2) + 1, j, mx1, mn1);
+        cout << mx << " " << mn << " " << mx1 << " " << mn1 << endl;
+        mx1 > mx? mx = mx1 : mx = mx;
+        mn1 < mn? mn = mn1 : mn = mn;
+    }
+}
 int main(){
     int n;
     cout << "How many elements you want to insert? : ";
@@ -9,12 +22,8 @@ int main(){
     int arr[n];
     for(int i = 0; i < n; i++) cin >> arr[i];
     // searching starts from here
-    int _max = arr[0], _min = _max;
-    for(int i = 1; i < n; i++){
-        if(arr[i] > _max) _max = arr[i];
-        if(arr[i] < _min) _min = arr[i];
-    }
-    cout << "Max element of the array is: " << _max << endl << "Min element of the array is: " << _min << endl;
+    fMaxMin(arr, 0, n - 1, mx, mn);
+    cout << "Max element of the array is: " << mx << endl << "Min element of the array is: " << mn << endl;
     return 0;
 }
 
